39de67f1481a69eeb759b8111c193050f8f7e760
fareed0915 <51469715+fareed0915@users.noreply.github.com>
2020-10-07 23:07:11 -0400

    Create IntegerSetException.java

---
+++ IntegerSetException.java
@@ -0,0 +1,19 @@
+public class IntegerSetException extends Exception {
+
+
+
+    public IntegerSetException() {
+
+	super();
+
+    }
+
+
+
+    public IntegerSetException(String s) {
+
+	super(s);
+
+    }
+
+}
